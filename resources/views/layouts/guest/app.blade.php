552ea969d93e75509db1c3e7d13637af1ea50b24
enthelotcloud <enthelotcloud@gmail.com>
2026-06-16 00:56:35 +0300

    fixed 500 the first - array without brackets

--- resources/views/layouts/guest/app.blade.php
+++ resources/views/layouts/guest/app.blade.php
@@ -4,8 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    @vite('resources/css/app.css','resources/js/app.js')
+    <title>{{$title ?? "Dj Smith Movies" }}</title>
+    <meta name="description" content={{$description ?? "watch all your favourite Dj smith Movies online for a monthly subscription."}}>
+    <link rel="icon" type="image/png" href="favicon-32x32.png">
+    <link rel="apple-touch-icon" type="image/png"  href="">
+    @vite(['resources/css/app.css','resources/js/app.js'])
     @livewireStyles
 </head>
 <body>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home')->name('home');
+Route::view('/about', 'about')->name('about');
+Route::view('/contact', 'contact')->name('contact');
+Route::view('/faqs', 'faqs')->name('faqs');
+Route::view('/privacy-policy', 'privacy-policy')->name('privacy');
+Route::view('/terms-of-services', 'terms-of-services')->name('terms');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
