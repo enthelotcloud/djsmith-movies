37bbede2d252bd39d365e859717fd579232bb4a6
enthelotcloud <enthelotcloud@gmail.com>
2026-07-10 13:55:08 +0300

    with csrf

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,7 +4,7 @@
 <title>
     {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
-
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <link rel="icon" href="/favicon.ico" sizes="any">
 <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 <link rel="apple-touch-icon" href="/apple-touch-icon.png">
